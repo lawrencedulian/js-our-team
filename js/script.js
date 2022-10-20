3724c17b806b5105ab30a6512f84742d6d2f6e9b
lawrencedulian <lawrenceedulian@gmail.com>
2022-10-20 19:08:35 +0200

    creato array object

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+
+// CREAZIONE OBJECT ARRAW CON INFORMAZIONI
+const membersTeam = [
+    {
+        name: "Wayne Barnett",
+        position: "Founder & CEO",
+        picture: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        position: "Chief Editor",
+        picture: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        position: "Office Manager",
+        picture: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angelo Lopez",
+        position: "Social Media Manager",
+        picture: "angelo-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        position: "Developer",
+        picture: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        position: "Graphic Designer",
+        picture: "barbara-ramos-graphic-designer.jpg"
+    }
+];
+// STAMPO NELLA CONSOLE L'ARRAY
+console.log(membersTeam);
+
